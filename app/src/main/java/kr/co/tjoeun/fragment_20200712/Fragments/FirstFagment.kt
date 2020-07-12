ece0ba919be70055451a88a2f95f7b265afaa317
김문기 <heartboy08@nate.com>
2020-07-12 14:36:37 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/fragment_20200712/Fragments/FirstFagment.kt
@@ -0,0 +1,8 @@
+package kr.co.tjoeun.fragment_20200712.Fragments
+
+import androidx.fragment.app.Fragment
+
+class FirstFagment : Fragment()
+{
+
+}
---
+++ app/src/main/java/kr/co/tjoeun/fragment_20200712/MainActivity.kt
@@ -0,0 +1,22 @@
+package kr.co.tjoeun.fragment_20200712
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setValues()
+        setupEvents()
+    }
+
+    override fun setValues() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setupEvents() {
+        TODO("Not yet implemented")
+    }
+}
