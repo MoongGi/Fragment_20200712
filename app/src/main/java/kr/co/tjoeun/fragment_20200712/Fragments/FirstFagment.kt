ae6ab65a00770ee929c8a382ce24497ed8e5db37
김문기 <heartboy08@nate.com>
2020-07-12 14:43:06 +0900

    프레그먼트 생성

--- app/src/main/java/kr/co/tjoeun/fragment_20200712/Fragments/FirstFagment.kt
+++ app/src/main/java/kr/co/tjoeun/fragment_20200712/Fragments/FirstFagment.kt
@@ -1,8 +1,20 @@
 package kr.co.tjoeun.fragment_20200712.Fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kr.co.tjoeun.fragment_20200712.R
 
 class FirstFagment : Fragment()
 {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
 }
